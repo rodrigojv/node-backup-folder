922bdc6f4dad2a6eecd1590ffa4725aa60fd9c00
Rodrigo Villalba <rodris.jvo@gmail.com>
2017-06-28 18:05:15 -0400

    First commit

---
+++ create.sh
@@ -0,0 +1,9 @@
+rm -r tmp/
+mkdir tmp
+touch tmp/text1.txt
+mkdir tmp/somedir
+mkdir tmp/backupthis
+touch tmp/backupthis/back.back
+echo "foo bar bazz" >> tmp/backupthis/back.back
+mkdir tmp/someotherdir
+touch tmp/text2.txt
